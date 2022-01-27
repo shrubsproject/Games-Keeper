7b66baa4b5cdeb502889c32679f19a7ae8a08031
epmvito <57107267+epmvito@users.noreply.github.com>
2022-01-27 17:06:26 +0300

    lkf koska 365

--- shrubsProject/controllers/gameViewController.swift
+++ shrubsProject/controllers/gameViewController.swift
@@ -80,6 +80,102 @@ final class gameViewController: UIViewController{
         diceView.isHidden = true
         diceView.addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(hideDice)))
         
+        leftButton.setImage(UIImage(named: "..."), for: .normal)
+        leftButton.addTarget(self, action: #selector(prePlayer), for: .touchUpInside)
+        
+        rightButton.setImage(UIImage(named: "..."), for: .normal)
+        rightButton.addTarget(self, action: #selector(back), for: .touchUpInside)
+        
+        collectionView.backgroundColor = UIColor.blue
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPagingEnabled = false
+        
+        timerLabel.font = UIFont(name: "Zapf Dignbats", size: 30)
+        timerLabel.textColor = UIColor.white
+        
+        playButton.setImage(UIImage(named: "..."), for: .normal)
+        playButton.addTarget(self, action: #selector(stopAndPlay), for: .touchUpInside)
+    }
+    
+    func configureLayout(){
+        let multiplier: CGFloat = (view.safeAreaLayoutGuide.layoutFrame.height / Constants.verticalHeight)
+        headerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    @objc func addButtonTap(sender: UIButton){
+        if sender == oneButton{
+            viewModel.add(score: 1)
+        }
+        if sender == buttons[0]{
+            viewModel.add(score: -10)
+        }
+        if sender == buttons[1]{
+            viewModel.add(score: -5)
+        }
+        if sender == buttons[2]{
+            viewModel.add(score: -1)
+        }
+        if sender == buttons[0]{
+            viewModel.add(score: 5)
+        }
+        if sender == buttons[0]{
+            viewModel.add(score: 10)
+        }
+        collectionView.reloadItems(at: [IndexPath(item: viewModel.currentPlayIndex, section: 0)])
+    }
+    
+    @objc func newGame(){
+        viewModel.newGame()
+    }
+    
+    @objc func showResults(){
+        viewModel.showResults()
+    }
+    
+    @objc func showDice(){
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        diceView.isHidden = false
+    }
+    
+    @objc func hideDice(){
+        diceView.isHidden = true
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    @objc func back(){
+        viewModel.resetTurn()
+        collectionView.reloadItems(at: [IndexPath(row: viewModel.currentPlayerIndex, section: 0)])
+    }
+    
+    @objc func prePlayer(){
+        if (viewModel.currentPlayerIndex == viewModel.players.count - 1){
+            viewModel.currentPlayerIndex = 0
+        }else{
+            viewModel.currentPlayerIndex += 1
+        }
+        scrollToCurrentIndex()
+    }
+    
+    @objc func stopAndPlay(){
+        if let timer = timer, timer.isValid {
+            timerLabel.textColor = UIColor.gray
+            playButton.setImage(UIImage(named: "..."), for: .normal)
+            stopTimer()
+        }else{
+            timerLabel.textColor = UIColor.white
+            playButton.setImage(UIImage(named: "..."), for: .normal)
+            startTimer()
+        }
+    }
+    
+    func stopTimer(){
+        stopTimer = Date.timeIntervalSinceReferenceDate
+        timer?.invalidate()
+    }
+    
+    func startTimer(){
         
     }
 }
