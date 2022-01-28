67b8f7b6db04d8406279e7a413876fa296707251
epmvito <57107267+epmvito@users.noreply.github.com>
2022-01-28 17:53:43 +0300

    lkz koska 6328

--- shrubsProject/controllers/resultViewController.swift
+++ shrubsProject/controllers/resultViewController.swift
@@ -5,4 +5,6 @@
 //  Created by Ilya Egorov on 02.01.2022.
 //
 
-import Foundation
+import UIKit
+
+
--- shrubsProject/custom view/shadowButton.swift
+++ shrubsProject/custom view/shadowButton.swift
@@ -12,4 +12,66 @@ class ShadowButton: UIButton {
     override var buttonType: UIButton.ButtonType {
         get{.custom}
     }
+    
+     var isShadow: Bool = false{
+        willSet{
+            if (newValue){
+                layer.shadowOpacity = 0.5
+            }else{
+                layer.shadowOpacity = 0.0
+            }
+        }
+    }
+    
+    override var isEnabled: Bool{
+        willSet{
+            if newValue{
+                alpha = 1.0
+            }else{
+                alpha = 0.6
+            }
+        }
+    }
+    
+    private var path: UIBezierPath?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+    
+    func configureUI(){
+        layer.masksToBounds = false
+        titleLabel?.layer.masksToBounds = false
+        
+        backgroundColor = UIColor.lightGray
+        
+        setTitleColor(.white, for: .normal)
+        setTitleColor(UIColor.white.withAlphaComponent(0.5), for: .highlighted)
+        
+        titleLabel?.layer.shadowOffset = CGSize(width: 0, height: 2)
+        titleLabel?.layer.shadowColor = UIColor(red: 0.33, green: 0.50, blue: 0.60, alpha: 1.0).cgColor
+        titleLabel?.layer.shadowOpacity = 0.5
+        titleLabel?.layer.shadowRadius = 0
+        
+        layer.shadowRadius = 0
+        layer.shadowColor = UIColor(red: 0.33, green: 0.50, blue: 0.60, alpha: 1.0).cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 5.0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height / 2
+        let path = UIBezierPath(roundedRect: bounds, cornerRadius: bounds.height / 2)
+        self.path = path
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        return path?.contains(point) ?? false
+    }
 }
