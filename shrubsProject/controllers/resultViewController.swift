d5eef496cba1accded6dc7ab2501558b8b28a82b
epmvito <57107267+epmvito@users.noreply.github.com>
2022-01-31 12:00:01 +0300

    kfldnbioeb

--- shrubsProject/controllers/resultViewController.swift
+++ shrubsProject/controllers/resultViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ResultViewController: UIViewController{
     
-    var viewModel: ResultViewModel!
+    var viewModel: ResultsViewModel!
     
     let scrollView = UIScrollView()
     let tableView = UITableView(frame: .zero, style: .plain)
@@ -49,9 +49,74 @@ final class ResultViewController: UIViewController{
         NSLayoutConstraint.activate([
             headerLabel.topAnchor.constraint(equalTo: headerLabel.topAnchor),
             headerLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.horizontalOfSet),
-            headerLabel.heightAnchor.constraint(equalTo: Constants.headerLabelHeight),
+            headerLabel.heightAnchor.constraint(equalToConstant: Constants.headerLabelHeight),
             
-            scrollView.topAnchor.constraint(equalTo:lkjgfgdhb)
+            scrollView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: viewHieght * Constants.spacingRatio),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.horizontalOfSet),
+            scrollView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            scrollView.heightAnchor.constraint(equalToConstant: viewHieght * Constants.scrollViewRatio),
+            
+            tableView.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: viewHieght * Constants.spacingRatio),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor , constant: Constants.horizontalOfSet),
+            tableView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Constants.spacingRatio * viewHieght)
         ])
+    }
+    
+    @objc func newGame(){
+        viewModel.newGame()
+    }
+    
+    @objc func resume(){
+        viewModel.resume()
+    }
+}
+
+extension ResultViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TurnID", for: indexPath)
+        cell.backgroundColor = UIColor.gray
+        
+        cell.textLabel?.text = viewModel.turns[indexPath.row].player
+        cell.textLabel?.font = UIFont(name: "...", size: 30)
+        cell.textLabel?.textColor = .white
+        
+        let rightLabel = cell.detailTextLabel!
+        rightLabel.font = UIFont(name: "...", size: 30)
+        rightLabel.textColor = .white
+        
+        let score = viewModel.turns[indexPath.row].scoreChange
+        let string = score > 0 ? "+\(score)" : "\(score)"
+        rightLabel.text = string
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.turns.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        Constants.cellHeight
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: Constants.cellHeight))
+        view.backgroundColor = UIColor.gray
+        
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+}
+
+extension ResultViewController{
+    enum Constants{
+        static let headerLabelHeight: CGFloat = 42
+        static let horizontalOfSet: CGFloat = 20
+        static let spacingRatio: CGFloat = 0.03
+        static let scrollViewRatio: CGFloat = 0.2
+        static let cellHeight: CGFloat = 50.0
     }
 }
--- shrubsProject/view model/resultViewModel.swift
+++ shrubsProject/view model/resultViewModel.swift
@@ -7,3 +7,36 @@
 
 import Foundation
 
+protocol ResultViewModelCoordin{
+    func newGameFromResults()
+    func resume()
+}
+
+class ResultsViewModel {
+    var players: [Player]
+    var turns: [Turn]
+    let coordin: ResultViewModelCoordin
+    
+    init(coordin: ResultViewModelCoordin, players: [Player], turns: [Turn]){
+        self.coordin = coordin
+        self.players = players
+        self.turns = turns.reversed()
+    }
+    
+    func newGame(){
+        coordin.newGameFromResults()
+    }
+    
+    func resume(){
+        coordin.resume()
+    }
+    
+    func scoreRank() -> [(Player, Int)]{
+        let players = players.sorted {
+            if $0.score > $1.score { return true }
+            if $0.score == $1.score, $0.name < $1.name { return true }
+            return false
+        }
+        return Array(zip(players, players.indices.map({$0.advanced(by: 1)})))
+    }
+}
